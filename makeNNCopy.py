e3f555d0d4b0271257b56efec864fb32813e5cee
zsmerritt <zsmerritt@gmail.com>
2019-03-15 09:04:55 -0700

    added model saving to makeNNCopy.py

--- makeNNCopy.py
+++ makeNNCopy.py
@@ -4,7 +4,7 @@ from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
 
-def main():
+def model1():
 	model = Sequential()
 	model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(150, 150, 3)))
 	model.add(Activation('relu'))
@@ -65,7 +65,96 @@ def main():
 	        epochs=50,
 	        validation_data=validation_generator,
 	        validation_steps=1000 // batch_size)
-	model.save_weights('first_try.h5') 
+	model.save_weights('first_try1.h5')
+	model.save('model1.dnn') 
+
+def model2():
+	model = Sequential()
+	model.add(Conv2D(64, kernel_size=(3, 3), input_shape=(150, 150, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(64, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+
+	model.add(Conv2D(128, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(128, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+
+	model.add(Conv2D(256, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(256, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+
+	model.add(Conv2D(512, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(512, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+
+	model.add(Conv2D(512, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(512, kernel_size=(3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+	model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+	model.add(Dense(4096))
+	model.add(Activation('relu'))
+	model.add(Dense(4096))
+	model.add(Activation('relu'))
+	model.add(Dense(4096))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(1))
+	model.add(Activation('sigmoid'))
+
+	model.compile(loss='binary_crossentropy',
+	              optimizer='rmsprop',
+	              metrics=['accuracy'])
+
+	batch_size = 16
+
+	# this is the augmentation configuration we will use for training
+	train_datagen = ImageDataGenerator(
+	        rescale=1./255,
+	        shear_range=0.2,
+	        zoom_range=0.2,
+	        horizontal_flip=True)
+
+	# this is the augmentation configuration we will use for testing:
+	# only rescaling
+	test_datagen = ImageDataGenerator(rescale=1./255)
+
+	# this is a generator that will read pictures found in
+	# subfolers of 'data/train', and indefinitely generate
+	# batches of augmented image data
+	train_generator = train_datagen.flow_from_directory(
+	        './data/train/',  # this is the target directory
+	        target_size=(150, 150),  # all images will be resized to 150x150
+	        batch_size=batch_size,
+	        class_mode='binary')  # since we use binary_crossentropy loss, we need binary labels
+
+	# this is a similar generator, for validation data
+	validation_generator = test_datagen.flow_from_directory(
+	        './data/testLabeled/',
+	        target_size=(150, 150),
+	        batch_size=batch_size,
+	        class_mode='binary')
+
+
+	model.fit_generator(
+	        train_generator,
+	        steps_per_epoch=25000 // batch_size,
+	        epochs=50,
+	        validation_data=validation_generator,
+	        validation_steps=1000 // batch_size)
+	model.save_weights('first_try2.h5')
+	model.save('model2.dnn') 
+
 
 if __name__ == '__main__':
 	main()
