e4aa88d9c6f5035d42df636c8e4931190b1a1315
zsmerritt <zsmerritt@gmail.com>
2019-03-16 01:22:27 -0700

    makeNNCopy.py: further lower gpu memory limit

--- makeNNCopy.py
+++ makeNNCopy.py
@@ -74,7 +74,7 @@ def model1():
 def model2():
 
 	config = tf.ConfigProto()
-	config.gpu_options.per_process_gpu_memory_fraction = 0.6
+	config.gpu_options.per_process_gpu_memory_fraction = 0.3
 	set_session(tf.Session(config=config))
 
 	image_size=160
