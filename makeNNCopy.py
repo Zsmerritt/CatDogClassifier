374819f51d42bb862897aa4df5698cdaacd15391
zsmerritt <zsmerritt@gmail.com>
2019-03-28 23:54:21 -0700

    fixed bug in makeNNCopy.py

--- makeNNCopy.py
+++ makeNNCopy.py
@@ -6,7 +6,7 @@ config.gpu_options.allow_growth = True
 config.gpu_options.visible_device_list = "0"
 set_session(tf.Session(config=config))
 
-
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense, BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
