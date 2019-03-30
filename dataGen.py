ad822b2741fc8c0ba2bf16bf2867134d3e78fbcc
zsmerritt <zsmerritt@gmail.com>
2019-03-29 23:50:22 -0700

    bug fix: changed from glob to os.walk for getting filenames

--- dataGen.py
+++ dataGen.py
@@ -264,7 +264,7 @@ def image_processor(transform_map, target_size,image_multiplier=1,save_test_imag
 
 def image_processor_batch(transform_map, target_size, batch_size):
 	#load file paths
-	image_paths = glob.glob(pathname=transform_map['data_folder']+'/**/*.jpg', recursive=True)
+	result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(transform_map['data_folder']) for f in filenames if os.path.splitext(f)[1] == '.jpg']
 	# Select files (paths/indices) for the batch
 	batch_paths = np.random.choice(a = image_paths, size = batch_size)
 	#lists to store data and labels
